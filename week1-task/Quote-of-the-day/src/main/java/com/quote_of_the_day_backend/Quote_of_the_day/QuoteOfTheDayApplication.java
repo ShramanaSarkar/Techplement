da1b89ebfcde1b66c0f908015350bb399f91e44a
ShramanaSarkar <shramana2000@gmail.com>
2025-03-22 11:39:05 +0530

    week 1 task
    week-1 task added to the repo

---
+++ week1-task/Quote-of-the-day/src/main/java/com/quote_of_the_day_backend/Quote_of_the_day/QuoteOfTheDayApplication.java
@@ -0,0 +1,13 @@
+package com.quote_of_the_day_backend.Quote_of_the_day;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteOfTheDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoteOfTheDayApplication.class, args);
+	}
+
+}
---
+++ week1-task/Quote-of-the-day/src/test/java/com/quote_of_the_day_backend/Quote_of_the_day/QuoteOfTheDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quote_of_the_day_backend.Quote_of_the_day;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteOfTheDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
